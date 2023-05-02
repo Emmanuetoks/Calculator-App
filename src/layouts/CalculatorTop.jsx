fbfd97d48ea347cd260acd585f0cf1695530baa2
ArmyofDeux <103246856+Emmanuetoks@users.noreply.github.com>
2023-05-02 13:35:21 -0700

    Modified the calculator screen

--- src/layouts/CalculatorTop.jsx
+++ src/layouts/CalculatorTop.jsx
@@ -4,16 +4,16 @@ import Header from '../components/Header'
 const CalculatorTop = ({ calcText, setCalcText }) => {
   const handleChange = (event) => {
     setCalcText(event.target.value)
-
-    console.log(event.target.value);
+    // console.log(event.target.value);
   }
   return (
     <>
       <div className='calculator__top'>
       <Header />
-        <div className='calculator__screen flex'>
+      <input value={calcText} onChange={handleChange} className='calculator__screen'/>
+        {/* <div className='calculator__screen flex'>
           <h1 className='calculator__screen-text'>{calcText}</h1>
-        </div>
+        </div> */}
       </div>
     </>
 
